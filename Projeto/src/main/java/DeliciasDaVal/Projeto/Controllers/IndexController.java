97990e3a63912d2ad1f7c6f932e3bccac701f9d6
maluel <57541772+maluel@users.noreply.github.com>
2019-11-29 14:26:38 -0300

    Criação do Projeto
    Criação de classes e pacotes

---
+++ Projeto/src/main/java/DeliciasDaVal/Projeto/Controllers/IndexController.java
@@ -0,0 +1,13 @@
+package DeliciasDaVal.Projeto.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Index";
+	}
+}
---
+++ Projeto/src/test/java/DeliciasDaVal/Projeto/ProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package DeliciasDaVal.Projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
